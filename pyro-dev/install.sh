fd5336f45bde8935b933a791bcc00bc6c5534bc5
James Webber <jamestwebber@users.noreply.github.com>
2018-11-21 22:14:29 -0800

    only install from git

--- pyro-dev/install.sh
+++ pyro-dev/install.sh
@@ -4,23 +4,16 @@ set -xe
 pip install --upgrade pip
 
 # 2. Install Pyro
-# Use pypi wheel if pyro_branch = 'release'.
-# Else, install from source, using git branch `pyro_branch`
-if [ ${pyro_branch} = "release" ]
-then
-    pip install --upgrade networkx tqdm opt_einsum graphviz
-    pip install pyro-ppl --no-deps
-else
-    git clone https://github.com/uber/pyro.git
-    (cd pyro && git checkout ${pyro_branch} && pip install .[dev])
-fi
-
+pip install --upgrade networkx tqdm opt_einsum graphviz
+git clone https://github.com/uber/pyro.git
+(cd pyro && git checkout ${pyro_branch} && pip install .[dev] --no-deps)
+cd ${HOME}
 
 # install jupyterlab, umap, altair
 conda install -y jupyterlab scikit-learn scipy numpy numba
 conda install -y altair -c conda-forge
 pip install umap-learn
 
-cd ${HOME}
 git clone https://github.com/czbiohub/simscity.git
-cd simscity && python setup.py install && cd -
+(cd simscity && python setup.py install)
+cd ${HOME}
